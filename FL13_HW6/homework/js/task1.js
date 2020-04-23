16af389195ae60ed70a2bc815fd29d6c76fc9291
sofyam1323 <sofyam1323@gmail.com>
2020-04-23 20:59:42 +0300

    HW6

--- FL13_HW6/homework/js/task1.js
+++ FL13_HW6/homework/js/task1.js
@@ -2,10 +2,10 @@ let check = prompt('What is your check number?', 0);
 let tip = prompt('What is tip percentage?', 0);
 let maxPercent = 100;
 let round = 100;
-let amount = check * tip / maxPercent;
+let amount = check * parseInt(tip) / maxPercent;
 let sum = +check + amount;
     check = Math.round(check * round) /round;
-    tip = Math.round(tip * round) /round;
+    tip = Math.round(parseInt(tip) * round) /round;
     amount = Math.round(amount * round) /round;
     sum = Math.round(sum * round) /round;
 if( check === null || tip === null || isNaN(check) || isNaN(tip) || check < 0 || tip < 0 || tip > maxPercent ) {
