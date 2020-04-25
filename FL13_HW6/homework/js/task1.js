e52915d9b3a60a9cef6b2d6ae8c8371beae194c9
sofyam1323 <sofyam1323@gmail.com>
2020-04-25 19:26:27 +0300

    HW6

--- FL13_HW6/homework/js/task1.js
+++ FL13_HW6/homework/js/task1.js
@@ -1,9 +1,10 @@
 let check = prompt('What is your check number?', 0);
+    check = parseInt(check);
 let tip = prompt('What is tip percentage?', 0);
 let maxPercent = 100;
 let round = 100;
 let amount = check * parseInt(tip) / maxPercent;
-let sum = +check + amount;
+let sum = check + amount;
     check = Math.round(check * round) /round;
     tip = Math.round(parseInt(tip) * round) /round;
     amount = Math.round(amount * round) /round;
