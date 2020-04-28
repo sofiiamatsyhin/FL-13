6b2e62d8180229e75c5a6d185bd120ca566b00b2
sofyam1323 <sofyam1323@gmail.com>
2020-04-28 20:53:16 +0300

    HW7

--- FL13_HW7/homework/src/js/task1.js
+++ FL13_HW7/homework/src/js/task1.js
@@ -1,29 +1,29 @@
 let login = prompt('Write your login', 'login');
 let password;
-let user = {
+let date = new Date();
+const USER = {
     'User' : 'UserPass',
     'Admin' : 'RootPass'
 };
-let date = new Date();
-let minSymbols = 4;
-let dayHoursFirst = 8;
-let dayHoursSecond = 20;
+const MIN_SYMBOL = 4;
+const DAY_HOURS_FIRST = 8;
+const DAT_HOURS_SECOND = 20;
 if ( login === null || login ==='') {
     alert('Canceled')
-} else if ( login.length < minSymbols ) {
+} else if ( login.length < MIN_SYMBOL ) {
     alert(`I don't know any users having name length less than 4 symbols`)
-} else if ( user[login] ) {
+} else if ( USER[login] ) {
     password = prompt('Write your password', 'password');
     if ( password === null || password === '') {
         alert('Canceled')
     } else if ( login === 'User' && password === 'UserPass' ) {
-        if ( dayHoursFirst < date.getHours() && dayHoursSecond > date.getHours() ) {
+        if ( DAY_HOURS_FIRST < date.getHours() && DAT_HOURS_SECOND > date.getHours() ) {
             alert('Good day, dear User!')
         } else {
             alert('Good evening, dear User!')
         }
     } else if ( login === 'Admin' && password === 'RootPass') {
-        if( dayHoursFirst < date.getHours() && dayHoursSecond > date.getHours() ) {
+        if( DAY_HOURS_FIRST < date.getHours() && DAT_HOURS_SECOND > date.getHours() ) {
             alert('Good day, dear Admin!')
         } else {
             alert('Good evening, dear Admin!')
