b3d8606d18b8a5e926abaa230c8fecd04a5a3643
Sofiia <matsyhin@Sofiias-MacBook-Air.local>
2020-05-14 17:19:36 +0300

    HW12

---
+++ FL13_HW12/homework/app.js
@@ -0,0 +1,45 @@
+const root = document.getElementById('root');
+
+let dataBook = JSON.parse(localStorage.getItem('data'))
+
+function Page() {
+
+    const createDiv = document.createElement('div');
+    const createMainHeader = document.createElement('h1');
+    const createSecondHeader = document.createElement('h2');
+    const createThirdHeader = document.createElement('h3');
+    const createLink = document.createElement('a');
+    const createPar = document.createElement('p');
+
+    createDiv.setAttribute('class', 'main');
+    createMainHeader.setAttribute('class', 'main_header');
+    createSecondHeader.setAttribute('class', 'main_second-header');
+    createThirdHeader.setAttribute('class', 'main_third-header');
+    createLink.setAttribute('class', 'main_link');
+    createPar.setAttribute('class', 'main_par');
+
+    document.querySelector('.main_header').innerHTML('Book List');
+
+    createDiv.appendChild(createMainHeader);
+    createDiv.appendChild(createSecondHeader);
+    createDiv.appendChild(createThirdHeader);
+    createDiv.appendChild(createLink);
+    createDiv.appendChild(createPar);
+
+}
+
+const WEBPAGE = new Page();
+
+window.addEventListener('hashchange', hashControl);
+
+function hashControl() {
+
+    if (window.location.hash === '1') {
+        WEBPAGE.firstBook();
+    } else if (window.location.hash === '2') {
+        WEBPAGE.secondBook();
+    } else if (window.location.hash === '3') {
+        WEBPAGE.thirdBook();
+    }
+    
+}
---
+++ FL13_HW12/homework/books.js
@@ -0,0 +1,28 @@
+const data = [
+    {
+        id: '1',
+        bookName: 'Nineteen Eighty-Four',
+        author: 'George Orwell',
+        image: 'https://en.wikipedia.org/wiki/File:1984first.jpg',
+        plot: `The story takes place in an imagined future, the year 1984,
+        when much of the world has fallen victim to perpetual war, omnipresent government surveillance,
+        historical negationism, and propaganda.`
+    },
+    {
+        id: '2',
+        bookName: 'Brave New World',
+        author: 'Aldous Huxley',
+        image: 'https://en.wikipedia.org/wiki/File:BraveNewWorld_FirstEdition.jpg',
+        plot: `Largely set in a futuristic World State, whose citizens are environmentally engineered
+        into an intelligence-based social hierarchy.`
+    },
+    {
+        id: '3',
+        bookName: 'A Clockwork Orange',
+        author: 'Anthony Burgess',
+        image: 'https://en.wikipedia.org/wiki/File:Clockwork_orange.jpg',
+        plot: `It is set in a near-future society that has a youth subculture of extreme violence.
+        The teenage protagonist, Alex, narrates his violent exploits 
+        and his experiences with state authorities intent on reforming him.`
+    }
+]
