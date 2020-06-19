3fb412dbaeace9fa3ae474457d0e4c21a44931f5
Sofiia <matsyhin@Sofiias-MacBook-Air.local>
2020-06-20 01:45:22 +0300

    HW02

--- FL_13_02_homework_es_next/homework/js/tasks.js
+++ FL_13_02_homework_es_next/homework/js/tasks.js
@@ -45,14 +45,27 @@ function add(num1 = required(), num2 = required()) {
 function fetchName() {
   return fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
-    .then(name => {
-      getName(name);
-      sortArr(name);
+    .then(json => {
+      sortArr(json);
+      getName(json);
     })
 }
+
 function getName(data) {
-  return data.forEach(person => console.log(person));
+  data.forEach(person => console.log(person.name));
 }
+
 function sortArr(data) {
-  return data.sort();
+  data.sort(function(a, b){
+    if(a.name < b.name) { return -1; }
+    if(a.name > b.name) { return 1; }
+    return 0;
+})
+}
+
+async function fetchAsync() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await response.json();
+  sortArr(data);
+  getName(data);
 }
